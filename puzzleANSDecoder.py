1c381008004a2d0abb91f587d39752ec39b0083a
TIK <firmament_solo@hotmail.com>
2020-01-10 00:31:31 +0800

    modified output structure

--- puzzleANSDecoder.py
+++ puzzleANSDecoder.py
@@ -1,5 +1,6 @@
 
-#JAN 1st 2020 T.I. created this file
+#JAN 10th 2020 T.I. modified output structure (RankedlineList1)
+#JAN 9th  2020 T.I. created this file
 
 fileName="inputSample1.txt"
 lineList1 = [line.rstrip('\n') for line in open(fileName) if (line.startswith('Across') is False) and (line.startswith('Down') is False)]
@@ -9,20 +10,16 @@ for line in lineList1:
     ke=line[0]
     RankedlineList1[int(ke)-1]=line
  
-print (lineList1)
-print (RankedlineList1)
-
-
-
 fileName="inputANS1.txt"
 lineList2 = [line.rstrip('\n') for line in open(fileName)]
 
-for line in lineList1:
+idx=0
+for line in RankedlineList1:
     lineT=line[line.index(' '):]
     for line2 in lineList2:
         lineT2=line2[line2.index(' '):]
         if lineT2==lineT:
-            lineT=lineT+":"+line2[:line2.index(' ')]
-            print (lineT)
-
-
+            lineListT=[line[:line.index('.')],line[line.index(' ')+1:],line2[:line2.index(' ')]]
+            RankedlineList1[idx]=lineListT
+    idx=idx+1
+print (RankedlineList1)
