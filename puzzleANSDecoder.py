9bbc70ac5286d2d25e1be2dfc03830f9059632af
TIK <firmament_solo@hotmail.com>
2020-01-10 08:17:31 +0800

    refactor

--- puzzleANSDecoder.py
+++ puzzleANSDecoder.py
@@ -1,4 +1,5 @@
 
+#JAN 10th 2020 T.I. refactor
 #JAN 10th 2020 T.I. modified output structure (RankedlineList1)
 #JAN 9th  2020 T.I. created this file
 
@@ -13,13 +14,13 @@ for line in lineList1:
 fileName="inputANS1.txt"
 lineList2 = [line.rstrip('\n') for line in open(fileName)]
 
-idx=0
-for line in RankedlineList1:
+for lineidx in range(len(RankedlineList1)):
+    line=RankedlineList1[lineidx]
     lineT=line[line.index(' '):]
     for line2 in lineList2:
         lineT2=line2[line2.index(' '):]
         if lineT2==lineT:
             lineListT=[line[:line.index('.')],line[line.index(' ')+1:],line2[:line2.index(' ')]]
-            RankedlineList1[idx]=lineListT
-    idx=idx+1
+            RankedlineList1[lineidx]=lineListT
+    
 print (RankedlineList1)
